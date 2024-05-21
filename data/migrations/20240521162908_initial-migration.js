0f0fac8edaa9360086714667dca45d300fb461de
Katy Kane <katytheartist@gmail.com>
2024-05-21 12:32:34 -0500

    wip

--- data/migrations/20240521162908_initial-migration.js
+++ data/migrations/20240521162908_initial-migration.js
@@ -5,13 +5,18 @@
 exports.up = async function(knex) {
     await knex.schema
         .createTable('recipes', table =>{
-            table.increments()
+            table.increments('recipe_id')
+            table.string('recipe_name', 200).notNullable().unique()
         })
         .createTable('ingredients', table =>{
-            table.increments()
+            table.increments('ingredient_id')
+            table.string('ingredient_name', 200).notNullable().unique()
+            table.string('ingredient_unit', 50)
         })
         .createTable('steps', table =>{
-            table.increments()
+            table.increments('step_id')
+            table.string('step_text', 200)
+            
         })
         .createTable('step_ingredients', table =>{
             table.increments()
