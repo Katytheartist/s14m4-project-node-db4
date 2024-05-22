0d372701acd17254a724d7afd5efee92def5a8f9
Katy Kane <katytheartist@gmail.com>
2024-05-22 17:25:46 -0500

    wip

--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -26,8 +26,8 @@ const step_ingredients = [
     {step_id: 1, ingredient_id: 1, quantity: 2},
     {step_id: 2, ingredient_id: 2, quantity: 1},
     {step_id: 3, ingredient_id: 5, quantity: 5},
-    {step_id: 1, ingredient_id: 3, quantity: 1},
-    {step_id: 2, ingredient_id: 4, quantity: 500},
+    {step_id: 6, ingredient_id: 3, quantity: 1},
+    {step_id: 7, ingredient_id: 4, quantity: 500},
 ]
 
 exports.seed = async function(knex){
